468e9c21a671eb609d95524a24e839319d6d28b4
karelbondan <karel.herdito@binus.ac.id>
2021-01-06 22:33:08 +0700

    less bugs

--- Wolf/backgroundtask.py
+++ Wolf/backgroundtask.py
@@ -1,38 +1,56 @@
 import time
-
 import pyautogui
 import speech_recognition
 import os
 import socket
+import pygetwindow
+import random  # for debugging
 
+# speech recognition init
 sr = speech_recognition.Recognizer()
-calling = ['woof woof', 'hey wolf', 'hey woof', 'wolf wolf', 'hello wolf', 'hey google', 'ok google']
+
+# prediction list
+calling = ['woof', 'wolf', 'wolves', 'walf', 'wilf', 'woof woof', 'wolf wolf', 'wall', 'well']
 
 
+# restore or activate (focus on) the program
+def check_window():
+    wolf = pygetwindow.getWindowsWithTitle('Wolf - Assistant')[0]
+    print(wolf)
+    print(wolf.isActive)
+    print(wolf.isMinimized)
+    if wolf.isMinimized:
+        wolf.restore()
+    if not wolf.isActive:
+        wolf.minimize()
+        wolf.restore()
+    else:
+        pass
+
+
+# main loop
+time.sleep(1.3)
 while True:
-    with open(f'C:/Wolf/users/{socket.gethostname()}/config.json') as mic_name:
-        microphone = eval(mic_name.read())
-        microphone = microphone['stylesheet']
+    try:
+        # socket.gethostname() gets the pc's name
+        with open(f'C:/Wolf/users/{socket.gethostname()}/config.json') as mic_name:
+            microphone = eval(mic_name.read())
+            microphone = microphone['stylesheet']
+            print(microphone)
+    except FileNotFoundError:
+        microphone = 'turqoise'  # the default style
         print(microphone)
 
+    # finding the mic button
     def find_mic():
         mic_attempt_one = pyautogui.locateOnScreen(f'C:/Wolf/bin/microphones/{microphone}.png', confidence=.9)
         return mic_attempt_one
 
 
-    def find_icon_light():
-        locate_light_taskbar = pyautogui.locateOnScreen('C:/Wolf/bin/taskbar_light.png', confidence=.9)
-        return locate_light_taskbar
-
-
-    def find_icon_dark():
-        locate_dark_taskbar = pyautogui.locateOnScreen('C:/Wolf/bin/taskbar_dark.png', confidence=.9, grayscale=False)
-        return locate_dark_taskbar
-
     try:
         with speech_recognition.Microphone() as input:
-            print('listening...')
-            audio_input = sr.listen(input, 2, 2)
+            print(f'{random.randint(0,100)} - listening...')
+            audio_input = sr.listen(input, 2.5, 2.5)  # input, waiting input timeout, speech timeout
 
             try:
                 audio_data = sr.recognize_google(audio_input).lower()
@@ -42,6 +60,8 @@ while True:
 
                     try:
                         mic_attempt_one = find_mic()
+
+                        # raises error when it doesn't find the mic button, else click the button
                         if mic_attempt_one == None:
                             raise FileNotFoundError
                         else:
@@ -49,22 +69,18 @@ while True:
 
                     except:
                         try:
-                            taskbar_light = find_icon_light()
-                            if taskbar_light == None:
-                                raise FileNotFoundError
-                            else:
-                                pyautogui.click(taskbar_light)
-
-                                mic = find_mic()
-                                pyautogui.click(mic)
-
-                        except:
-                            dark_taskbar = find_icon_dark()
-                            pyautogui.click(dark_taskbar)
-
+                            check_window()
                             mic = find_mic()
                             pyautogui.click(mic)
-                    time.sleep(13)
+
+                        except:  # attempt the second time when it fails to find the window
+                            check_window()
+                            mic = find_mic()
+                            pyautogui.click(mic)
+
+                    # pausing to let the assistant program execute first; this program isn't in sync
+                    time.sleep(5)
+
                 else:
                     print("i'm not called yet... waiting to be called...")
 
