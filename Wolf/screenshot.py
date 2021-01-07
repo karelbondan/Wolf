c4b2373abe14b35d00a8afc01bbacf0ca39255b8
karelbondan <karel.herdito@binus.ac.id>
2021-01-08 00:18:01 +0700

    added false prediction

--- Wolf/screenshot.py
+++ Wolf/screenshot.py
@@ -3,16 +3,26 @@ import os
 import re
 import pygetwindow
 
+# prediction list if user wants to ss the screen
 predictions = ['take', 'a', 'screenshot', 'my', 'the', 'screen', 'window', 'app']
+
+# if these indexes are in the search input then user is not trying to ss the screen
+abort = ['how', 'search', 'to']
 
 
 def screenshot():
     images = ''
-    os.chdir(f'{os.environ["USERPROFILE"]}\\Pictures\\Screenshots')  # change dir to ss default location
+
+    # change dir to ss default location, listing the existing screenshots
+    os.chdir(f'{os.environ["USERPROFILE"]}\\Pictures\\Screenshots')
     for image in os.listdir():
         images += f'{image} '
-    screenshots = re.findall(r'\s*\((\d*)\)', images)  # finding the numbers
-    latest_screenshot = [int(screenshot) for screenshot in screenshots]  # converting numbers to int
+
+    # finding the numbers
+    screenshots = re.findall(r'\s*\((\d*)\)', images)
+
+    # converting numbers to int
+    latest_screenshot = [int(screenshot) for screenshot in screenshots]
 
     # getting the highest number, increment by 1, screenshot
     pyautogui.screenshot(f'Screenshot ({max(latest_screenshot) + 1}).png')
@@ -49,6 +59,9 @@ def check_userinput(input):
     for prediction in user_input:
         if prediction in predictions:
             counter += 1
+        elif prediction in abort:  # if any of abort's indexes are in user input then abort
+            counter = 0
+            break
         else:
             pass
     return counter
